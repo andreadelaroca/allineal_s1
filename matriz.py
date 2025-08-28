1316842744333ea25c3481885efd12fabfd4c09e
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-28 05:34:46 -0600

    Ejercicio finalizado

--- matriz.py
+++ matriz.py
@@ -1,9 +1,21 @@
-matriz = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]]
+filas = int(input("Ingrese el número de filas: "))
+columnas = int(input("Ingrese el número de columnas: "))
 
-for linea in matriz:
-    for elemento in linea:
-        print(elemento, end=" ")
-    print()
-        
+matriz = []
+
+for i in range(filas):
+    fila_actual = [] # lista vacía para manejar fila actual
+    for j in range(columnas):
+        elemento = input(f"Introduzca el elemento {i + 1},{j + 1}: ")
+        fila_actual.append(elemento)
+    matriz.append(fila_actual)
+    
+if filas == columnas:
+    print(f"La matriz es cuadrada.", end = " ")
+else:
+    print("La matriz es rectangular.", end = " ")    
+       
+print(f"Su tamaño es {filas}x{columnas}")
+
+for fila in matriz:
+    print(fila)
