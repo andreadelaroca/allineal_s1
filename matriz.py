29f9baaeed1a7153c2e3b3fb94ff3206d7f9d29b
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-28 05:11:20 -0600

    Matriz integrada

--- matriz.py
+++ matriz.py
@@ -1,1 +1,9 @@
-# generar matrices a partir de inputs del usuario
+matriz = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+for linea in matriz:
+    for elemento in linea:
+        print(elemento, end=" ")
+    print()
+        
